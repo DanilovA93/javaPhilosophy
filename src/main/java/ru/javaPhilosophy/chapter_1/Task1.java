e33fe9e76b3f4c09ffbc2b6de2cd322f51cc1306
Anton Danilov <danilovanton1993@mail.ru>
2019-02-12 14:29:44 +0300

    first chapter

---
+++ src/main/java/ru/javaPhilosophy/chapter_1/Task1.java
@@ -0,0 +1,15 @@
+package ru.javaPhilosophy.chapter_1;
+
+public class Task1 {
+    static int num;
+    static char ch;
+
+    public Task1(){
+        System.out.println("num - \"" + num + "\"");
+        System.out.println("ch - \"" + ch + "\"");
+    }
+
+    public static void main(String[] args) {
+        new Task1();
+    }
+}
